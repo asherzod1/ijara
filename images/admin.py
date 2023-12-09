ed456de0f81487e82dbf1569bc6cf8df45a8f2bb
Abdumutalov <sh.abdumutalov1@gmail.com>
2023-12-09 19:38:09 +0500

    12

--- images/admin.py
+++ images/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
-from images.models import Images
-
-
 # Register your models here.
-
-@admin.register(Images)
-class ImagesAdmin(admin.ModelAdmin):
-    list_display_links = ["id"]
-    list_display = ["uuid", "name"]
